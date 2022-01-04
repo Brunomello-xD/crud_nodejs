1599182799c2d47e2659ad10d32b90e52cea59a3
Brunomello-xD <brunomello.ti@protonmail.com>
2022-01-04 00:46:35 -0300

    add update video

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,3 @@
-import { GetAllVideosController } from './controllers/GetAllVideosController';
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
 import { GetAllCateroriesController } from './controllers/GetAllCategoriesController';
@@ -6,6 +5,8 @@ import { DeleteCategoryController } from './controllers/DeleteCategoryController
 import { UpdateCategoryController } from './controllers/UpdateCategoryController';
 import { CreateVideoController } from './controllers/CreateVideoController';
 import { DeleteVideosController } from './controllers/DeleteVideosController';
+import { UpdateVideoController } from './controllers/UpdateVideoController';
+import { GetAllVideosController } from './controllers/GetAllVideosController';
 
 const routes = Router();
 
@@ -22,5 +23,6 @@ routes.put("/categories/:id", new UpdateCategoryController().handle);
 routes.post("/videos", new CreateVideoController().handle);
 routes.get("/videos", new GetAllVideosController().handle);
 routes.delete("/videos/:id", new DeleteVideosController().handle);
+routes.put("/videos/:id", new UpdateVideoController().handle)
 
 export { routes }
